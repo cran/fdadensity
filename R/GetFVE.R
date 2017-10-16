f214e779585071909d8e444788554029323ca415
Alexander Petersen <petersen@pstat.ucsb.edu>
2017-10-16 17:13:00 +0000

    version 0.1.0

---
+++ R/GetFVE.R
@@ -0,0 +1,80 @@
+#' Function for converting densities to quantile functions 
+#' 
+#' @param fpcaObj PACE output (FPCA on LQDs)
+#' @param dmatrix matrix of original densities measures on grid dSup, rows correspond to individual densities
+#' @param dSup support for Density domain - max and min mark the boundary of the support. 
+#' @param metric metric for measuring variance - 'L2' for Euclidean or 'W' for Wasserstein
+#' @param useAlpha should regularisation be performed to densities in dmatrix? (default = FALSE)
+#' @param alpha scalar to deregularise when transforming back to density space with (default = 0.01)
+#' 
+#' @return FVEvector
+#' 
+#' @examples 
+#' 
+#' data(Top50BabyNames)
+#'
+#' # Perform Transformation FPCA for male baby name densities
+#' dSup = Top50BabyNames$x
+#' X = FPCAdens(dmatrix = t(Top50BabyNames$dens$male), dSup = dSup, useAlpha = TRUE, 
+#'                    optns = list(dataType = 'Dense', error = FALSE, methodSelectK = 8))
+#'
+#' # Compute FVE - must compare to regularized densities 
+#' 
+#' fveL2 = GetFVE(fpcaObj = X, dmatrix = t(Top50BabyNames$dens$male), dSup = dSup, useAlpha = TRUE)
+#' fveW = GetFVE(fpcaObj = X, dmatrix = t(Top50BabyNames$dens$male), dSup = dSup, 
+#'                    metric = 'W', useAlpha = TRUE)
+#' 
+#' @seealso \code{\link{RegulariseByAlpha},\link{lqd2quantile}}
+#' 
+#' @references
+#' \cite{Functional Data Analysis for Density Functions by Transformation to a Hilbert space, Alexander Petersen and Hans-Georg Mueller, 2016} 
+#' @export
+
+GetFVE = function(fpcaObj, dmatrix, dSup, metric = 'L2', useAlpha = FALSE, alpha=0){
+  
+  if(!(metric %in% c('L2', 'W'))){
+    stop('Unrecognized value for metric input.')
+  }
+  
+  if(useAlpha){
+    tmp = t(apply(dmatrix, 1, function(d) RegulariseByAlpha(dSup, d)))
+    dmatrix = tmp
+  }
+  
+  if(metric == 'L2'){
+    mu_dens = colMeans(dmatrix)
+    #pointsToKeep <-  which(0 < apply(dmatrix, 2, quantile, 0.995))
+  
+    vtot = mean(apply(dmatrix, 1, function(u) trapzRcpp(X = dSup, Y = (u - mu_dens)^2)))
+    K = length(fpcaObj$lambda)
+    FVEs = rep(0, K)
+    # print(K)
+  
+    for(k in 1:K){
+      #print(k)
+      fittedKQ <- fitted(K=k, fpcaObj);
+      fittedKD <- MakeDENsample(fittedKQ, alpha = alpha, dSup = dSup, useAlpha = useAlpha) 
+      vK <- mean(apply((dmatrix -  fittedKD$DEN)^2, 1, function(u) trapzRcpp(X = dSup, Y = u)))
+      FVEs[k] <- (vtot - vK)/vtot 
+    }
+  }else {
+    
+    Qmatrix = t(apply(dmatrix, 1, function(d) dens2quantile(d, dSup)))
+    mu_Q = colMeans(Qmatrix)
+    
+    vtot = mean(apply(Qmatrix, 1, function(u) trapzRcpp(X = dSup, Y = (u - mu_Q)^2)))
+    K = length(fpcaObj$lambda)
+    FVEs = rep(0, K)
+    
+    for(k in 1:K){
+      fittedK.lqd <- fitted(K=k, fpcaObj);
+      fittedKQ <- t(apply(fittedK.lqd, 1, function(y) lqd2quantile(lqd = y, lb = dSup[1])))
+      vK <- mean(apply((Qmatrix -  fittedKQ)^2, 1, function(u) trapzRcpp(X = dSup, Y = u)))
+      FVEs[k] <- (vtot - vK)/vtot  
+    }
+    
+    
+  }
+ 
+  return(FVEs)
+}
---
+++ R/dens2lqd.R
@@ -0,0 +1,43 @@
+#' Function for converting densities to log quantile density functions 
+#' 
+#' @param dens density values on dSup - must be strictly positive and integrate to 1
+#' @param dSup support (grid) for Density domain
+#' @param lqdSup support for lqd domain - must begin at 0 and end at 1; default [0,1] with N-equidistant support points
+#' @param N desired number of points on a [0,1] grid for lqd function; default length(dSup)
+#' 
+#' @return lqd log quantile density on lqdSup
+#' 
+#' @examples
+#' x <- seq(0,2,length.out =512)
+#' y <- rep(0.5,length.out =512)
+#' y.lqd <- dens2lqd( dens=y, dSup = x) # should equate # log(2)
+#' @references
+#' \cite{Functional Data Analysis for Density Functions by Transformation to a Hilbert space, Alexander Petersen and Hans-Georg Mueller, 2016} 
+#' @export
+
+dens2lqd = function(dens, dSup, N = length(dSup), lqdSup = NULL){
+
+  if(is.null(lqdSup)){
+      lqdSup = seq(0, 1, length.out = N)
+  }
+
+  # Check density requirements
+  if(any(dens<=0)){
+    stop('Please correct negative or zero probability density estimates.')
+  }  
+  if(abs( trapzRcpp(X = dSup, dens) - 1) > 1e-5){
+    stop('Density does not integrate to 1 with tolerance of 1e-5 - please adjust.')
+  }
+  if(!all.equal( range(lqdSup),c(0,1) )){
+    stop("Please check the support of the LQD domain's boundaries.")
+  }
+ 
+  # Get CDF  
+  qtemp = cumtrapzRcpp(X = dSup, dens)
+  # ind = duplicated(qtemp)
+  # qtemp = unique(qtemp)
+  # lqd_temp = -log(dens[!ind]);
+  lqd_temp = -log(dens)
+  lqd = spline(x = qtemp, y = lqd_temp, xout = lqdSup)$y
+  return(lqd)
+}
